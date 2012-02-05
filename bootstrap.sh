5b28152865e83fea0b3c4c014a53461afc3e501c
Kalon Hinds <kh.cubed@gmail.com>
2012-02-04 21:10:40 -0500

    simple symlinking script

---
+++ bootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+CONFIG_PATH=`dirname $0`
+FILES="$CONFIG_PATH/.*"
+index=0
+
+for file in $FILES; do
+    filename=`basename $file`
+
+    if [ "$index" -gt "1" ] && [ "$filename" != ".git" ]; then
+        `ln -s $file ~/$filename`
+    fi
+
+    let index++
+done
